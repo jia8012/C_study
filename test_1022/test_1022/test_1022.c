f43a589209ce54244bb88832de1c774f96ef4d24
JHJ <845776779@qq.com>
2019-10-23 22:53:41 +0800

    学习提交

--- test_1022/test_1022/test_1022.c
+++ test_1022/test_1022/test_1022.c
@@ -1,24 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 
-int main() {
-	int num = 1;
-	int i = 0;
-	for ( num = 1; num < 1000; num++)
-	{
-		int j = 0;//存放因子的和
-		for (i = 1; i < num ; i++)
-		{
-			if (num%i == 0)
-			{
-				j += i;
-			}
+//int main() {
+//	int num = 1;
+//	int i = 0;
+//	for ( num = 1; num < 1000; num++)
+//	{
+//		int j = 0;//存放因子的和
+//		for (i = 1; i < num ; i++)
+//		{
+//			if (num%i == 0)
+//			{
+//				j += i;
+//			}
+//
+//		}
+//		if (num == j)
+//		{
+//			printf("%d ", num);
+//		}
+//	}
+//	return 0;
+//}
 
-		}
-		if (num == j)
-		{
-			printf("%d ", num);
-		}
+
+int main()
+{
+	int a = 1;
+	int b = 1;
+	int i = 1;
+	for (i = 1; i <= 20; i++)
+	{
+		printf("%d\n%d\n", a, b);
+		a = a + b;
+		b = a + b;
 	}
 	return 0;
 }
